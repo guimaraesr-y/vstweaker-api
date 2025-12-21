39837163fe588a9a28b5621e28e562739c7cf1da
Ryan Guimarães <guimaraesryan26@gmail.com>
2025-12-21 13:34:59 -0300

    Remove comments

--- manager/api/controller.py
+++ manager/api/controller.py
@@ -32,25 +32,16 @@ def upload_vs(request, file: UploadedFile = File(...)):
     return VSFile.objects.get(id=entity.id)
 
 
-# ---------------------------------------------
-# 1. LISTAR VS
-# ---------------------------------------------
 @router.get("/", response=List[VSFileSchema])
 def list_vs(request):
     return VSFile.objects.all()
 
 
-# ---------------------------------------------
-# 2. LISTAR AUDIO TRACKS DE UM VS
-# ---------------------------------------------
 @router.get("/{vs_id}/tracks", response=List[AudioTrackSchema])
 def list_vs_tracks(request, vs_id: int):
     return AudioTrack.objects.filter(vs_file_id=vs_id)
 
 
-# ---------------------------------------------
-# 3. DETALHES DE UM VS + SUAS TRACKS
-# ---------------------------------------------
 @router.get("/{vs_id}", response=VSFileDetailSchema)
 def get_vs_detail(request, vs_id: int):
     vs = get_object_or_404(VSFile, id=vs_id)
