06ece2c84e7659e6b3253110e5af5e9ec73dd425
Ryan Guimarães <guimaraesryan26@gmail.com>
2025-12-27 00:58:58 -0300

    Fix localstorage strategy

--- manager/infra/storage.py
+++ manager/infra/storage.py
@@ -10,9 +10,7 @@ class FileStorage:
 class LocalStorage(FileStorage):
     def __init__(self, base_path=None):
         self.base_path = base_path or os.path.join(settings.MEDIA_ROOT, "vs_files")
-
-        if not os.path.exists(self.base_path):
-            os.makedirs(self.base_path)
+        os.makedirs(self.base_path, exist_ok=True)
 
     def save(self, filename: str, content) -> str:
         path = os.path.join(self.base_path, filename)
